b1a78c33a3a5239d10ee80c073bd4bfc8e504dce
step4me <prosikeffect@gmail.com>
2017-05-27 15:14:40 +0900

    Add Fragments

--- app/src/main/java/kr/co/midas/midasmobile/MainActivity.java
+++ app/src/main/java/kr/co/midas/midasmobile/MainActivity.java
@@ -2,31 +2,30 @@ package kr.co.midas.midasmobile;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.TabHost;
 
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import kr.co.midas.midasmobile.base.domain.Issue;
-import kr.co.midas.midasmobile.base.network.GithubIssue;
+import kr.co.midas.midasmobile.base.fragments.MyHome;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    @BindView(R.id.testText)
-    TextView mTextView;
+//    @BindView(R.id.testText)
+//    TextView mTextView;
+
+    private TabHost tabHost;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +34,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ButterKnife.bind(this);  /// 중요!!! Acitivity 마다 안써주면 Crash 발생합니다
+//        ButterKnife.bind(this);  /// 중요!!! Acitivity 마다 안써주면 Crash 발생합니다
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -56,20 +55,26 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        GithubIssue.getGitIssueListAsync("square", "retrofit", new GithubIssue.RetrofitCallback() {
-            @Override
-            public void success(List<Issue> gitIssues) {
+//        GithubIssue.getGitIssueListAsync("square", "retrofit", new GithubIssue.RetrofitCallback() {
+//            @Override
+//            public void success(List<Issue> gitIssues) {
+//
+//                for(int i=0;i<10;i++){
+//                    mTextView.append(gitIssues.get(i).toString());
+//                }
+//            }
+//
+//            @Override
+//            public void error(Throwable throwable) {
+//                Log.e("Retrofit Network", throwable.getMessage());
+//            }
+//        });
 
-                for(int i=0;i<10;i++){
-                    mTextView.append(gitIssues.get(i).toString());
-                }
-            }
+        MyHome main = new MyHome();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_content, main).commit();
 
-            @Override
-            public void error(Throwable throwable) {
-                Log.e("Retrofit Network", throwable.getMessage());
-            }
-        });
     }
 
     @Override
---
+++ app/src/main/java/kr/co/midas/midasmobile/base/fragments/MyHome.java
@@ -0,0 +1,127 @@
+package kr.co.midas.midasmobile.base.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabContentFactory;
+
+import java.util.List;
+import java.util.Vector;
+
+import butterknife.ButterKnife;
+import kr.co.midas.midasmobile.R;
+import kr.co.midas.midasmobile.base.adapters.MyFragmentPagerAdapter;
+
+public class MyHome extends Fragment implements OnTabChangeListener,
+        OnPageChangeListener {
+
+//	@BindView(R.id.tabHost)
+	TabHost tabHost;
+//	@BindView(R.id.viewPager)
+	ViewPager viewPager;
+	MyFragmentPagerAdapter myViewPagerAdapter;
+	int i = 0;
+	View view;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+		view = inflater.inflate(R.layout.tabs_viewpager_layout, container, false);
+
+		i++;
+		ButterKnife.bind(this.view);
+		// tabhost 설정
+		this.initializeTabHost(savedInstanceState);
+		// ViewPager 설정
+		this.initializeViewPager();
+
+		return view;
+	}
+
+	// fake content for tabhost
+	class FakeContent implements TabContentFactory {
+		private final Context mContext;
+
+		public FakeContent(Context context) {
+			mContext = context;
+		}
+
+		@Override
+		public View createTabContent(String tag) {
+			View v = new View(mContext);
+			v.setMinimumHeight(0);
+			v.setMinimumWidth(0);
+			return v;
+		}
+	}
+
+	private void initializeViewPager() {
+		List<Fragment> fragments = new Vector<Fragment>();
+
+		fragments.add(new RankFragment());
+		fragments.add(new VoluntFragment());
+		fragments.add(new TeamFragment());
+		fragments.add(new DonateFragment());
+
+
+		this.myViewPagerAdapter = new MyFragmentPagerAdapter(
+				getChildFragmentManager(), fragments);
+		this.viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+		this.viewPager.setAdapter(this.myViewPagerAdapter);
+		this.viewPager.setOnPageChangeListener(this);
+
+	}
+
+	private void initializeTabHost(Bundle args) {
+
+		tabHost = (TabHost) view.findViewById(R.id.tabHost);
+		tabHost.setup();
+
+		for (int i = 1; i <= 4; i++) {
+
+			TabHost.TabSpec tabSpec;
+			tabSpec = tabHost.newTabSpec("Tab " + i);
+			tabSpec.setIndicator("Tab " + i);
+			tabSpec.setContent(new FakeContent(getActivity()));
+			tabHost.addTab(tabSpec);
+		}
+		tabHost.setOnTabChangedListener(this);
+	}
+
+	@Override
+	public void onTabChanged(String tabId) {
+		int pos = this.tabHost.getCurrentTab();
+		this.viewPager.setCurrentItem(pos);
+
+		HorizontalScrollView hScrollView = (HorizontalScrollView) view
+				.findViewById(R.id.hScrollView);
+		View tabView = tabHost.getCurrentTabView();
+		int scrollPos = tabView.getLeft()
+				- (hScrollView.getWidth() - tabView.getWidth()) / 2;
+		hScrollView.smoothScrollTo(scrollPos, 0);
+
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+		this.tabHost.setCurrentTab(position);
+	}
+}
